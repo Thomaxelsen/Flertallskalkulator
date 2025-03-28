aa2e54383193336981ca2f7124c49ff88559c8ab
Thomaxelsen <thomas.axelsen@gmail.com>
2025-03-28 22:37:25 +0100

    Update navigationConfig.js

--- js/navigationConfig.js
+++ js/navigationConfig.js
@@ -1,7 +1,9 @@
-// Filsti: js/navigationConfig.js
+// navigationConfig.js - Sentral konfigurasjon for navigasjon
+// Denne filen inneholder alle navigasjonslenker for nettstedet
 
 // Definer alle navigasjonslenker i denne sentrale konfigurasjonsfilen
 window.navigationConfig = {
+    // Array av lenker som skal vises i navigasjonen
     navLinks: [
         { href: "index.html", title: "Flertallskalkulator" },
         { href: "party-overview.html", title: "Partioversikt" },
@@ -10,3 +12,5 @@ window.navigationConfig = {
         // Legg til nye sider her i fremtiden
     ]
 };
+
+console.log("Navigation config loaded");
