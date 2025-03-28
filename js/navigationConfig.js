f83062a4c3ce39642c276e5ca7b6ded11ad9fd20
Thomaxelsen <thomas.axelsen@gmail.com>
2025-03-28 22:20:20 +0100

    Create navigationConfig.js

---
+++ js/navigationConfig.js
@@ -0,0 +1,12 @@
+// Filsti: js/navigationConfig.js
+
+// Definer alle navigasjonslenker i denne sentrale konfigurasjonsfilen
+window.navigationConfig = {
+    navLinks: [
+        { href: "index.html", title: "Flertallskalkulator" },
+        { href: "party-overview.html", title: "Partioversikt" },
+        { href: "majority-coalitions.html", title: "Flertallskoalisjoner" },
+        { href: "issue-matrix.html", title: "Saksmatrise" }
+        // Legg til nye sider her i fremtiden
+    ]
+};
