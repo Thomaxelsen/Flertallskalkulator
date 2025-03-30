82cf0a9adb5dc586c9c9252375c43fd4283cbf68
Thomaxelsen <thomas.axelsen@gmail.com>
2025-03-30 18:33:09 +0200

    Update navigationConfig.js

--- js/navigationConfig.js
+++ js/navigationConfig.js
@@ -5,12 +5,14 @@
 window.navigationConfig = {
     // Array av lenker som skal vises i navigasjonen
     navLinks: [
+        // Viktig: Bytt rekkefølge om ønskelig. Den første brukes ofte som "hjem".
         { href: "index.html", title: "Flertallskalkulator" },
         { href: "party-overview.html", title: "Partioversikt" },
         { href: "majority-coalitions.html", title: "Flertallskoalisjoner" },
         { href: "issue-matrix.html", title: "Saksmatrise" }
-        // Legg til nye sider her i fremtiden
+        // Legg til nye sider her i fremtiden, f.eks.:
+        // { href: "om-prosjektet.html", title: "Om Prosjektet" }
     ]
 };
 
-console.log("Navigation config loaded");
+console.log("Navigation config loaded:", window.navigationConfig.navLinks.length, "links defined."); // Litt mer informativ logg
